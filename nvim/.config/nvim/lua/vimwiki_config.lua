c854bd842f15e63b5c1a5e486c06dd16d9fb557f
Yves Bode <ynrbode@gmail.com>
2021-11-20 14:28:03 -0300

    Nov 20 14:28 2021

--- nvim/.config/nvim/lua/vimwiki_config.lua
+++ nvim/.config/nvim/lua/vimwiki_config.lua
@@ -1,1 +1,7 @@
-vim.cmd("let g:vimwiki_list = [{'path': '~/documents/wiki', 'path_html': '~/documents/wiki_html'}]")
+vim.g.vimwiki_list = {{
+    path = '~/documents/wiki',
+    path_html = '~/documents/wiki_html',
+    nested_syntaxes = {
+      javascript = 'javascript'
+    }
+}}
